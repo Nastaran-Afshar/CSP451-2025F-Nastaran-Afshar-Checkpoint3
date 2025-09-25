37f3092b82fbdeac5a8fa36aea4b4bffb16c98fa
nastaran-afshar <nastaranmohammadiafshar@yahoo.com>
2025-09-24 21:26:16 -0400

    applied prettier to jest.config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,18 @@
 // jest.config.cjs
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // 👇 tell Jest to treat .js as ESM (since package.json has "type": "module")
-  extensionsToTreatAsEsm: [".js"],
+  extensionsToTreatAsEsm: ['.js'],
   transform: {}, // no Babel needed for plain JS
 
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text-summary", "lcov"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
   coverageThreshold: {
-    global: { branches: 80, functions: 80, lines: 80, statements: 80 }
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   },
 
   // (optional) limit to your source files
-  collectCoverageFrom: ["src/**/*.js"]
+  collectCoverageFrom: ['src/**/*.js'],
 };
