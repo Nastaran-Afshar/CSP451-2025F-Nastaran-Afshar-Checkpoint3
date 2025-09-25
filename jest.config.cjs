30afe3f5ebf90486d3918003a531111e33154552
nastaran-afshar <nastaranmohammadiafshar@yahoo.com>
2025-09-24 21:44:12 -0400

    applied neccassry changes for codecov

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,18 +1,11 @@
-// jest.config.cjs
 module.exports = {
   testEnvironment: 'node',
-
-  // 👇 tell Jest to treat .js as ESM (since package.json has "type": "module")
-  extensionsToTreatAsEsm: ['.js'],
-  transform: {}, // no Babel needed for plain JS
-
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
   coverageThreshold: {
     global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   },
-
-  // (optional) limit to your source files
-  collectCoverageFrom: ['src/**/*.js'],
+  testMatch: ['**/*.test.mjs', '**/*.test.js'], // picks up .mjs tests too
 };
